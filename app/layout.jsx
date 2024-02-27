9221bd7bbfef726cd605a4aecff1c9e96ac84d92
shubham kumar <shubhofficial1@gmail.com>
2024-02-28 01:08:21 +0530

    Layout,HomePage & MetaData

---
+++ app/layout.jsx
@@ -0,0 +1,20 @@
+import "@/assets/styles/globals.css";
+
+export const metadata = {
+  title: "Quest",
+  description:
+    "Quest is an app made with nextjs & tailwind css for helping travellers travel acrosss the beautiful landcapes of the world",
+  keywords: "quest , travel , holiday",
+};
+
+const MainLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+};
+
+export default MainLayout;
