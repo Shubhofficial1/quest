ef842bb5c22b33be0eb53d1f6e39fac94323064d
shubham kumar <shubhofficial1@gmail.com>
2024-03-01 02:15:22 +0530

    User & Property Model

---
+++ models/propertyModel.js
@@ -0,0 +1,92 @@
+import mongoose from "mongoose";
+
+const PropertySchema = new mongoose.Schema(
+  {
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+    },
+    location: {
+      street: {
+        type: String,
+      },
+      city: {
+        type: String,
+      },
+      state: {
+        type: String,
+      },
+      zipcode: {
+        type: String,
+      },
+    },
+    beds: {
+      type: Number,
+      required: true,
+    },
+    baths: {
+      type: Number,
+      required: true,
+    },
+    square_feet: {
+      type: Number,
+      required: true,
+    },
+    ameneties: [
+      {
+        type: String,
+      },
+    ],
+    rates: {
+      nightly: {
+        type: Number,
+      },
+      weekly: {
+        type: Number,
+      },
+      monthly: {
+        type: Number,
+      },
+    },
+
+    seller_info: {
+      name: {
+        type: String,
+      },
+      emails: {
+        type: String,
+      },
+      phone: {
+        type: String,
+      },
+    },
+    images: [
+      {
+        type: String,
+      },
+    ],
+    is_featured: {
+      type: Boolean,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Property = mongoose.model("Property", PropertySchema);
+
+export default Property;
