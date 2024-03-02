f708c647c852c9fb69c8e39733ebb9d52f8d7b4b
shubham kumar <shubhofficial1@gmail.com>
2024-03-03 01:27:49 +0530

    Add Property Form & Component State

--- app/properties/add/page.jsx
+++ app/properties/add/page.jsx
@@ -1,8 +1,14 @@
+import PropertyAddForm from "@/components/PropertyAddForm";
+
 const AddPropertyPage = () => {
   return (
-    <div>
-      <h1>Add Property Page</h1>
-    </div>
+    <section className="bg-blue-50">
+      <div className="container max-w-2xl py-24 m-auto">
+        <div className="px-6 py-8 m-4 mb-4 bg-white border rounded-md shadow-md md:m-0">
+          <PropertyAddForm />
+        </div>
+      </div>
+    </section>
   );
 };
 
