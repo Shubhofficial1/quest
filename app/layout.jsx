87797241cab2d48c625204bef94d659e85243070
shubham kumar <shubhofficial1@gmail.com>
2024-03-02 17:30:14 +0530

    Added Session Provider Wrapper & added 3 env variables

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,6 +2,7 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
 import "@/assets/styles/globals.css";
+import AuthProvider from "@/components/AuthProvider";
 
 export const metadata = {
   title: "Quest | Enjoy your Stay",
@@ -12,13 +13,15 @@ export const metadata = {
 
 const HomeLayout = ({ children }) => {
   return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <main>{children}</main>
-        <Footer />
-      </body>
-    </html>
+    <AuthProvider>
+      <html lang="en">
+        <body>
+          <Navbar />
+          <main>{children}</main>
+          <Footer />
+        </body>
+      </html>
+    </AuthProvider>
   );
 };
 
